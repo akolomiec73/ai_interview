911779636112c87b5416cf32e7d2c04ec463ba37
Господин <fiesto73@icloud.com>
2026-04-22 18:30:46 +0400

    Добавлен метод сохрани анализа вакансии в бд

--- app/Repositories/Contract/EventRepositoryInterface.php
+++ app/Repositories/Contract/EventRepositoryInterface.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Repositories\Contract;
 
+use App\DTO\AnalyzedVacancyDto;
 use App\DTO\EventDto;
 use App\Models\Event;
+use App\Models\Vacancy;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Carbon;
 
@@ -14,4 +16,6 @@ interface EventRepositoryInterface
     public function createEvent(EventDto $data): Event;
 
     public function getEvents(Carbon $startDate, Carbon $endDate): Collection;
+
+    public function createVacancy(string $url, AnalyzedVacancyDto $aiResponse, int $eventId): Vacancy;
 }
--- app/Repositories/EventRepository.php
+++ app/Repositories/EventRepository.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\DTO\AnalyzedVacancyDto;
 use App\DTO\EventDto;
 use App\Models\Event;
+use App\Models\Vacancy;
 use App\Repositories\Contract\EventRepositoryInterface;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Carbon;
@@ -33,5 +35,26 @@ class EventRepository implements EventRepositoryInterface
             ->select('id', 'dateInterview', 'linkVacantion', 'comment')
             ->orderBy('dateInterview', 'asc')
             ->get();
+    }
+
+    /**
+     * Сохранение анализа вакансии от Ai
+     */
+    public function createVacancy(string $url, AnalyzedVacancyDto $aiResponse, int $eventId): Vacancy
+    {
+        $vacancy = Vacancy::create(
+            [
+                'url' => $url,
+                'company' => $aiResponse->company,
+                'salary' => $aiResponse->salary,
+                'format_work' => $aiResponse->formatWork,
+                'skills' => $aiResponse->skills,
+                'top_questions' => $aiResponse->topQuestions,
+            ]
+        );
+        Event::where('id', $eventId)->update(['vacancy_id' => $vacancy->id]);
+        \Log::info('try update key', ['eventId' => $eventId, 'vacancyId' => $vacancy->id]);
+
+        return $vacancy;
     }
 }
