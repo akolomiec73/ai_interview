189af87035312a391a2ff87352ab7a0d1dfeb667
Господин <fiesto73@icloud.com>
2026-04-24 15:52:42 +0400

    + роут для веба со страницей ивента

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,9 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\DayController;
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('main');
-});
-
+Route::get('/', fn () => view('main'))->name('main');
 Route::get('/day/{date}', [DayController::class, 'show'])->name('day.show');
+Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
